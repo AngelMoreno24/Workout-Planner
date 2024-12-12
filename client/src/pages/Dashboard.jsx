42290444b6d2a7f46bec508a188203331583d5f3
AngelMoreno24 <angel.moreno8424@gmail.com>
2024-12-12 15:39:46 -0700

    created css for cards in dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,8 +4,84 @@ import "./css/Dashboard.css"
 const Home = () => {
   
   return (
-    <div>
-      <p className='card'>asdasd</p>
+
+    <div className='center'>
+      <div className='grid-layout'>
+        
+        <div className='card'>
+          <div className='grid-addWorkout'>
+            
+            <p>category</p>
+            <input type="text" />
+            <p>name</p>
+            <input type="text" />
+            <p>sets</p>
+            <input type="text" />
+            <p>reps</p>
+            <input type="text" />
+            <p>weight</p>
+            <input type="text" />
+            <p>time</p>
+            <input type="text" />
+          </div>
+        </div>
+
+        
+        <div className='card'>
+          <div className='grid-addWorkout'>
+            
+            <p>category</p>
+            <input type="text" />
+            <p>name</p>
+            <input type="text" />
+            <p>sets</p>
+            <input type="text" />
+            <p>reps</p>
+            <input type="text" />
+            <p>weight</p>
+            <input type="text" />
+            <p>time</p>
+            <input type="text" />
+          </div>
+        </div>
+        
+        <div className='card'>
+          <div className='grid-addWorkout'>
+            
+            <p>category</p>
+            <input type="text" />
+            <p>name</p>
+            <input type="text" />
+            <p>sets</p>
+            <input type="text" />
+            <p>reps</p>
+            <input type="text" />
+            <p>weight</p>
+            <input type="text" />
+            <p>time</p>
+            <input type="text" />
+          </div>
+        </div>
+
+        <div className='card'>
+          <div className='grid-addWorkout'>
+            
+            <p>category</p>
+            <input type="text" />
+            <p>name</p>
+            <input type="text" />
+            <p>sets</p>
+            <input type="text" />
+            <p>reps</p>
+            <input type="text" />
+            <p>weight</p>
+            <input type="text" />
+            <p>time</p>
+            <input type="text" />
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
