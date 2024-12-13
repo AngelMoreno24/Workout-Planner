2fe8d0ff99ae03f79096e8e021a925f3663e4cee
AngelMoreno24 <angel.moreno8424@gmail.com>
2024-12-13 15:21:37 -0700

    added calender to workout page

--- client/src/pages/Workouts.jsx
+++ client/src/pages/Workouts.jsx
@@ -1,20 +1,42 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
+import "./css/Workouts.css"
+import { addWorkout } from '../api/workout'
 
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 const Workouts = () => {
 
 
-  useEffect( () => {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = currentDate.getMonth() + 1; // January is 0
-    const day = currentDate.getDate();
-  
-  console.log(`${year}-${month}-${day}`); // Output: 2024-12-11
-  })
+  const [value, setValue] = React.useState(dayjs('2022-04-17'));
 
+  useEffect(()=>{
+    console.log(value.$y);
+    console.log(value.$M+1);
+    console.log(value.$D);
+  },[value])
   return (
-    <div>
-      
+
+    <div className='center'>
+      <div className='grid-layout'>
+        
+        <div className='card'>
+
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+          
+            <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} />
+          </LocalizationProvider>
+        </div>
+
+        
+
+        <div className='card'>
+
+          <h2>Add new Workout</h2>
+        </div>
+      </div>
+
     </div>
   )
 }
